afc1ceab0367076f92c626e6d791a2ae7ac00a2c
Mohammed Karimullah <mohammed03sk9@gmail.com>
2024-10-15 19:55:02 +0900

    Add files via upload

---
+++ _6_greatest_of_2nums.js
@@ -0,0 +1,12 @@
+/*
+
+Given two integer inputs, the objective is to find the largest number among the two integer inputs. 
+In order to do so we usually use if-else statements to check which one’s greater.
+
+*/
+
+const greatestOfTwoNums = (a,b) => {
+    console.log(Math.max(a,b));
+}
+
+greatestOfTwoNums(14,7);
