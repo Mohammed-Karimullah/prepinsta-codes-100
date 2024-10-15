7b779499632ce439fd24d08dd058b26376c482b4
Mohammed Karimullah <mohammed03sk9@gmail.com>
2024-10-15 19:44:44 +0900

    Update _1_positive_and_negative.js

--- _1_positive_and_negative.js
+++ _1_positive_and_negative.js
@@ -1,3 +1,9 @@
+/*
+
+Given an integer input, the objective is check whether the given integer is Positive or Negative. 
+
+*/
+
 const numPosNeg = (num) => {
     console.log( num >= 0 ? num +  " is positive number" : num + " is Negative number");
 }
