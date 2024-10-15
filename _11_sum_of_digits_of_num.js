29a3ac0cc864b1063292e83e7ff5f2c89f0cf6c2
Mohammed Karimullah <mohammed03sk9@gmail.com>
2024-10-15 19:43:43 +0900

    Update _11_sum_of_digits_of_num.js

--- _11_sum_of_digits_of_num.js
+++ _11_sum_of_digits_of_num.js
@@ -1,3 +1,11 @@
+/*
+
+Given a number as an input the objective is to calculate the sum of the digits of the number. 
+We first break down the number into digits and then add all the digits to the sum variable. 
+
+*/
+
+
 const sumOfDigitsOfNum = (num) => {
     let res =  num
     .toString()
