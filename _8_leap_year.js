3caec1f2609b88fcde9c7c99a7c323c60ab4b872
Mohammed Karimullah <mohammed03sk9@gmail.com>
2024-10-15 19:39:25 +0900

    Update _8_leap_year.js

--- _8_leap_year.js
+++ _8_leap_year.js
@@ -1,3 +1,16 @@
+/*
+
+Given an integer input as the year, the objective is to Check if a Year is a Leap Year or Not
+
+Conditions for a Leap Year
+Here are the two conditions that any year must satisfy to be called a leap year
+1. The year must be perfectly divisible by 400.
+2. The number must be divisible by 4 but not by 100.
+
+*/
+
+
+
 const leapYr = (year) => {
     console.log(year % 400 === 0 ? `${year} : Leap year` : year % 4 === 0 && year % 100 !== 0 ? `${year} : Leap year` : `${year} : Not a leap year`);
 }
