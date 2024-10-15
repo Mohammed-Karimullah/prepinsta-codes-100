e7e25495db7cbda3fdbf2a24e854fa43cb542825
Mohammed Karimullah <mohammed03sk9@gmail.com>
2024-10-15 19:40:40 +0900

    Update _7_greatest_of_3nums.js

--- _7_greatest_of_3nums.js
+++ _7_greatest_of_3nums.js
@@ -1,3 +1,12 @@
+/*
+
+Given three integers as inputs the objective is to find the greatest among them. 
+In order to do so we check and compare the three integer inputs with each other 
+and which ever is the greatest we print that number.
+
+*/
+
+
 const greatestOf3Nums = (n1,n2,n3) => {
     console.log(n1 >= n2  && n1>= n3?  n1 : n2 >= n1 && n2 >= n3? n2 : n3);
 }
